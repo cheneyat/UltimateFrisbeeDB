e8a191434e2cff7370f76053f11bd927853d9dbd
August B Dailey <daileyab@rose-hulman.edu>
2020-01-30 20:10:49 -0500

    Add point ui fields

--- src/ui/Application.java
+++ src/ui/Application.java
@@ -16,6 +16,7 @@ public class Application {
 		frame.setResizable(false);
 		frame.setTitle("Ultimate Frisbee Stat Tracking App");
 		addTeamGuiStuff();
+		addPointGuiStuff();
 		frame.setLayout(null);
 		frame.setVisible(true);
 	}
@@ -49,6 +50,65 @@ public class Application {
 	
 	public void addPointGuiStuff() {
 		
+		
+		// Offense
+		JLabel offteamLabel = new JLabel();
+		offteamLabel.setText("Off Team");
+		offteamLabel.setBounds(20,400, 200, 30);
+		
+		JTextField offteamTextField = new JTextField();
+		offteamTextField.setEditable(true);
+		offteamTextField.setBounds(120, 400, 200, 30);
+		
+		frame.add(offteamTextField);
+		frame.add(offteamLabel);
+		
+		// Defense
+		JLabel defteamLabel = new JLabel();
+		defteamLabel.setText("Def Team");
+		defteamLabel.setBounds(420,400, 200, 30);
+		
+		JTextField defteamTextField = new JTextField();
+		defteamTextField.setEditable(true);
+		defteamTextField.setBounds(520, 400, 200, 30);
+		
+		frame.add(defteamTextField);
+		frame.add(defteamLabel);
+		
+		// Scoring Player
+		JLabel scoringPlayerLabel = new JLabel();
+		scoringPlayerLabel.setText("Scoring Player");
+		scoringPlayerLabel.setBounds(20,450, 200, 30);
+		
+		JTextField scoringPlayerTextField = new JTextField();
+		scoringPlayerTextField.setEditable(true);
+		scoringPlayerTextField.setBounds(120, 450, 200, 30);
+		
+		frame.add(scoringPlayerTextField);
+		frame.add(scoringPlayerLabel);
+		
+		// Assisting Player
+		JLabel assistingPlayerLabel = new JLabel();
+		assistingPlayerLabel.setText("Assisting Player");
+		assistingPlayerLabel.setBounds(420,450, 200, 30);
+		
+		JTextField assistingPlayerTextField = new JTextField();
+		assistingPlayerTextField.setEditable(true);
+		assistingPlayerTextField.setBounds(520, 450, 200, 30);
+		
+		frame.add(assistingPlayerLabel);
+		frame.add(assistingPlayerTextField);
+		
+		
+		JButton defteamButton = new JButton();
+		defteamButton.setText("Add Point");
+		defteamButton.setEnabled(true);
+		defteamButton.setBounds(550, 500, 150, 30);
+		
+		
+		
+		
+		frame.add(defteamButton);
 	}
 	
 
