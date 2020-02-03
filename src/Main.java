8fb042f7944e1b38b833548ec1e12b1bc986cac1
August B Dailey <daileyab@rose-hulman.edu>
2020-02-03 13:51:13 -0500

    Integrate properties wrapper

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Properties;
+
+import ploading.PWrapper;
 import sodabase.ui.ApplicationRunner;
 import uf.services.DatabaseConnectionService;
 import uf.services.PlayerService;
@@ -8,9 +11,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		DatabaseConnectionService dbService = new DatabaseConnectionService("golem.csse.rose-hulman.edu","UltimateFrisbeeDatabase20");
-		
-		if (dbService.connect("cheneyat", "")) {
+		PWrapper pwrap = new PWrapper("src/app.properties");
+		DatabaseConnectionService dbService = new DatabaseConnectionService(pwrap.getValue("serverName"), pwrap.getValue("databaseName"));
+		if (dbService.connect(pwrap.getValue("serverUsername"), pwrap.getValue("serverPassword"))) {
 			System.out.println("Connected to db");
 		}
 		
